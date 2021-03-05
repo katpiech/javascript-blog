442b77422d3cb0b40818e5f73b70ef3cfa75ef83
Katarzyna Piechocińska <piechocinska.katarzyna@gmail.com>
2021-03-05 14:57:29 +0100

    Generate HTML of the link and add to html variable & insert html of all the links into the tags wrapper

--- js/script.js
+++ js/script.js
@@ -86,17 +86,22 @@ function generateTags() {
     let articleTags = article.getAttribute('data-tags');
     console.log(articleTags);
     /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
 
     /* START LOOP: for each tag */
-
-    /* generate HTML of the link */
-
-    /* add generated code to html variable */
-
+    for (let tag of articleTagsArray) {
+      console.log(tag);
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      console.log(linkHTML);
+      /* add generated code to html variable */
+      html = html + '  ' + linkHTML;
+      console.log(html);
     /* END LOOP: for each tag */
-
+    }
     /* insert HTML of all the links into the tags wrapper */
-
+    tagsWrapper.innerHTML = html;
     /* END LOOP: for every article: */
   }
 }
