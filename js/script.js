c736139f8af9b7b658a178ad89c839acaa7fec1e
Katarzyna Piechocińska <piechocinska.katarzyna@gmail.com>
2021-03-05 20:51:54 +0100

    Add prevent default action for tagClickHandler event

--- js/script.js
+++ js/script.js
@@ -110,7 +110,7 @@ generateTags();
 
 function tagClickHandler(event){
   /* prevent default action for this event */
-
+  event.preventDefault();
   /* make new constant named "clickedElement" and give it the value of "this" */
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
